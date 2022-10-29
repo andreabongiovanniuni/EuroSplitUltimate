36c1f5fa13772b4cff5cc900bc52dff6296df2eb
andreabongiovanniuni <111112507+andreabongiovanniuni@users.noreply.github.com>
2022-10-29 12:14:46 +0200

    I'm a baaaaad guy
    duh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,14 @@
 import GUI.LaunchPageGUI;
 import basics.Group;
-import database.DBManager;
 import database.ESdatabase;
-
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
 
         Group g = new Group();
-        /** HELP */
-        ESdatabase pickyEatersDB = new ESdatabase("eurosplitdatas.sqlite");
+        /* HELP  */
+        ESdatabase DbEuroSplit = new ESdatabase("eurosplitdatas.sqlite");
 
         new LaunchPageGUI(g);
 
