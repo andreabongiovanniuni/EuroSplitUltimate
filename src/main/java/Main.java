e9f94ced4b5b3852a947d83a0ce5edf02e7d4b4c
andreabongiovanniuni <111112507+andreabongiovanniuni@users.noreply.github.com>
2022-10-29 12:01:33 +0200

    Working on database: insert

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         Group g = new Group();
+        /** HELP */
         ESdatabase pickyEatersDB = new ESdatabase("eurosplitdatas.sqlite");
 
         new LaunchPageGUI(g);
